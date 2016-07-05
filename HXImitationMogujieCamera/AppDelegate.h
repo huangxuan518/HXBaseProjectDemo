56221e6b243c2f61d5d5297ae838bfa72d7e855c
huangxuan <770493410@qq.com>
2016-07-05 15:26:26 +0800

    Initial Commit

---
+++ HXImitationMogujieCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXImitationMogujieCamera
+//
+//  Created by 黄轩 on 16/7/5.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXImitationMogujieCamera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HXImitationMogujieCamera
+//
+//  Created by 黄轩 on 16/7/5.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
